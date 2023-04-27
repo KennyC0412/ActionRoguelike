d2e3ece4369e66e726028a5144930c9680151cb8
KennyC <377545253@qq.com>
2023-04-27 21:25:58 +0800

    Add character and input

--- Source/ActionRoguelike/Private/ACTCharacter.cpp
+++ Source/ActionRoguelike/Private/ACTCharacter.cpp
@@ -2,13 +2,19 @@
 
 
 #include "ACTCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 AACTCharacter::AACTCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
+	SpringArmComponent->SetupAttachment(RootComponent);
+	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+	CameraComponent->SetupAttachment(SpringArmComponent);
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +22,16 @@ void AACTCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void AACTCharacter::MoveForward(float value)
+{
+	AddMovementInput(GetActorForwardVector(),value);
+}
+
+void AACTCharacter::MoveRight(float value)
+{
+	//AddMovementInput(GetACtor(),value);
 }
 
 // Called every frame
@@ -29,6 +45,10 @@ void AACTCharacter::Tick(float DeltaTime)
 void AACTCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward",this,&AACTCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("Turn",this,&APawn::AddControllerYawInput);
+	//PlayerInputComponent->BindAxis("MoveRight",this,&AACTCharacter::MoveRight);
 
 }
 
--- Source/ActionRoguelike/Public/ACTCharacter.h
+++ Source/ActionRoguelike/Public/ACTCharacter.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Character.h"
 #include "ACTCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class ACTIONROGUELIKE_API AACTCharacter : public ACharacter
 {
@@ -16,8 +19,17 @@ public:
 	AACTCharacter();
 
 protected:
+
+	UPROPERTY(VisibleAnywhere,Category="View")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(VisibleAnywhere,Category="View")
+	USpringArmComponent* SpringArmComponent;
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	void MoveForward(float value);
+	void MoveRight(float value);
 
 public:	
 	// Called every frame
